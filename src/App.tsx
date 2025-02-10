5a8fd5ed2a1bad95afb278fc656451b518ca0f09
Durin Tasondock <temgoua484@gmail.com>
2025-02-10 08:00:48 +0100

    Refactor CSS and add infinite scrolling feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,69 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [pages, setPages] = useState([0, 1])
+  const scrollRef = useRef<HTMLDivElement>(null)
+  const wrapperRef = useRef<HTMLDivElement>(null)
+  const prevScrollY = useRef<number>(0)
+
+  const handleScroll = () => {
+    const scrollPosition = scrollRef.current?.scrollTop || 0;
+
+    const scrollHeight = scrollRef.current?.scrollHeight || 0;
+    const viewportHeight = window.innerHeight;
+
+    const scrollLimit = scrollHeight - viewportHeight - 5;
+    
+    if(scrollPosition >= prevScrollY.current) {
+      if ((scrollPosition-5) % scrollLimit === 0) {
+        setPages(pages => [...pages, pages.length]);
+      }
+    } else {
+      setPages([0, 1])
+    }
+
+    prevScrollY.current = scrollPosition;
+  };
+
+  useEffect(() => {
+    return handleScroll()
+  }, [])
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <div className='App' ref={scrollRef} onScroll={handleScroll}>
+      <div ref={wrapperRef} className='Wrapper'>
+
+    {pages.map(() => 
+      <div className='Content'>
+        <div>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    )}
+      </div>
+    </div>
   )
 }
 
